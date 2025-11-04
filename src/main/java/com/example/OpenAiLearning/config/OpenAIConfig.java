1d38db69ac54a7b601bd36af7573b4a4d2b6b13c
Vimal Boopathi D <vimalboopathid.official11@gmail.com>
2025-11-04 18:03:46 +0530

    structured code by removed unused imports

--- src/main/java/com/example/OpenAiLearning/config/OpenAIConfig.java
+++ src/main/java/com/example/OpenAiLearning/config/OpenAIConfig.java
@@ -1,8 +1,6 @@
 package com.example.OpenAiLearning.config;
 
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
--- src/main/java/com/example/OpenAiLearning/controller/ImageGenController.java
+++ src/main/java/com/example/OpenAiLearning/controller/ImageGenController.java
@@ -5,7 +5,6 @@ import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
-import org.springframework.util.MimeType;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
